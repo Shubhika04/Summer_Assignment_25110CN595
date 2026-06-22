277e877266ad20435a7d10420bc3db63af896947
Shubhika04 <shubhikasuman04@gmail.com>
2026-06-22 21:30:51 +0530

    Day21 assignment is uploaded

---
+++ Day21/code81.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class code81 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        int count = 0;
+
+        for (char c : str.toCharArray()) {
+            count++;
+        }
+
+        System.out.println("The length of the string = " + count);
+
+        sc.close();
+    }
+}
---
+++ Day21/code83.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class code83 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        int vowels = 0;
+        int consonants = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+                ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+                vowels++;
+            }
+            else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+                      consonants++;
+            }
+        }
+
+        System.out.println("No. of Vowels = " + vowels);
+        System.out.println("No. of Consonants = " + consonants);
+
+        sc.close();
+    }
+}
